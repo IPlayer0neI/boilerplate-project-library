88b6df3e60ea32615951d7aaf9a219166ea000a9
IPlayer0neI <101530455+IPlayer0neI@users.noreply.github.com>
2023-02-20 19:31:56 -0300

    Add files via upload

--- server.js
+++ server.js
@@ -8,6 +8,7 @@ require('dotenv').config();
 const apiRoutes         = require('./routes/api.js');
 const fccTestingRoutes  = require('./routes/fcctesting.js');
 const runner            = require('./test-runner');
+const myDb              = require("./mongooseConnect.js")
 
 const app = express();
 
@@ -17,6 +18,26 @@ app.use(cors({origin: '*'})); //USED FOR FCC TESTING PURPOSES ONLY!
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
+app.route("/api/books")
+
+   .post(function(req, res, next){
+      if(req.body.title){
+        next();
+      }else{
+        res.send("missing required field title");
+      };
+   });
+
+app.route("/api/books/:id")
+
+  .post(function(req, res, next){
+    if(req.body.comment){
+      next()
+    }else{
+      res.send("missing required field comment")
+    }
+  })
+
 
 //Index page (static HTML)
 app.route('/')
@@ -28,7 +49,9 @@ app.route('/')
 fccTestingRoutes(app);
 
 //Routing for API 
-apiRoutes(app);  
+myDb(function(Books){
+  apiRoutes(app, Books);
+}); 
     
 //404 Not Found Middleware
 app.use(function(req, res, next) {
